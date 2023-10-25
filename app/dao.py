b1887aa8f7c9b1d9ddaeb0e76f2ea0fe468d614e
mtrunghieu12@gmail.com <mtrunghieu12@gmail.com>
2023-10-25 17:00:04 +0700

    init project

---
+++ app/dao.py
@@ -0,0 +1,48 @@
+def get_categories():
+    return [{
+        'id': 1,
+        'name': 'Mobile'
+    },
+    {
+        'id': 2,
+        'name': 'Tablet'
+    }
+    ]
+
+def get_products():
+    products = [{
+        'id':1,
+        'name':'iPhone 13',
+        'price':'20000000',
+        'image': "https://zit.ng/cdn/shop/files/WhatsAppImage2023-09-25at12.04.11_2048x2048.jpg?v=1695640194"
+    },
+        {
+            'id': 2,
+            'name': 'iPhone 13 Pro',
+            'price': '20000000',
+            'image':"https://www.google.com/imgres?imgurl=https%3A%2F%2Fbizweb.dktcdn.net%2F100%2"
+        },
+        {
+            'id': 3,
+            'name': 'iPhone 14',
+            'price': '20000000',
+            'image':"https://www.google.com/imgres?imgurl=https%3A%2F%2Fbizweb.dktcdn.net%2F100%2"
+        },{
+            'id': 4,
+            'name': 'iPhone 14 Pro',
+            'price': '20000000',
+            'image':"https://www.google.com/imgres?imgurl=https%3A%2F%2Fbizweb.dktcdn.net%2F100%2"
+        },{
+            'id': 5,
+            'name': 'iPhone 15',
+            'price': '20000000',
+            'image':"https://www.google.com/imgres?imgurl=https%3A%2F%2Fbizweb.dktcdn.net%2F100%2"
+        },{
+            'id': 6,
+            'name': 'iPhone 15 Pro',
+            'price':'20000000',
+            'image': "https://www.google.com/aclk?sa=l&ai=DChcSEwj9srD08JCCAxVNyxYFHZ38CnUYABAJGgJ0bA&ase=2&gclid=EAIaIQobChMI_bKw9PCQggMVTcsWBR2d_Ap1EAQYBSABEgKxvvD_BwE&sig=AOD64_003wOBpiGh94HOk4mrEvCzhzpghA&ctype=5&nis=5&adurl&ved=2ahUKEwj9gaj08JCCAxWDl1YBHU4hDc8Qvhd6BQgBEIcB"
+        }
+    ]
+
+    return products
