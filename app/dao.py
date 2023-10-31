ac2b62f3dfd9e7d8ff3821c4385e76e08f0b22e3
Your name <mtrunghieu12@gmail.com>
2023-11-01 00:26:56 +0700

    message

--- app/dao.py
+++ app/dao.py
@@ -9,40 +9,42 @@ def get_categories():
     }
     ]
 
-def get_products():
+def get_products(kw):
     products = [{
         'id':1,
         'name':'iPhone 13',
         'price':'20000000',
-        'image': "https://zit.ng/cdn/shop/files/WhatsAppImage2023-09-25at12.04.11_2048x2048.jpg?v=1695640194"
+        'image': "https://synnexfpt.com/wp-content/uploads/2022/07/Apple-iPhone-13-mini-2.jpg"
     },
         {
             'id': 2,
             'name': 'iPhone 13 Pro',
             'price': '20000000',
-            'image':"https://www.google.com/imgres?imgurl=https%3A%2F%2Fbizweb.dktcdn.net%2F100%2"
+            'image':"https://synnexfpt.com/wp-content/uploads/2022/06/iphone-13-pro-max-xanh-1.jpg"
         },
         {
             'id': 3,
             'name': 'iPhone 14',
             'price': '20000000',
-            'image':"https://www.google.com/imgres?imgurl=https%3A%2F%2Fbizweb.dktcdn.net%2F100%2"
+            'image':"https://synnexfpt.com/wp-content/uploads/2022/06/Dien-thoai-di-dong-Apple-iPhone-15.jpg"
         },{
             'id': 4,
             'name': 'iPhone 14 Pro',
             'price': '20000000',
-            'image':"https://www.google.com/imgres?imgurl=https%3A%2F%2Fbizweb.dktcdn.net%2F100%2"
+            'image':"https://product.hstatic.net/200000409445/product/14-max-tim_6dc05b654a3943fb8e9b03883b223d04_master.jpg"
         },{
             'id': 5,
             'name': 'iPhone 15',
             'price': '20000000',
-            'image':"https://www.google.com/imgres?imgurl=https%3A%2F%2Fbizweb.dktcdn.net%2F100%2"
+            'image':"https://admin.hoanghamobile.com/Uploads/2023/09/14/vn-iphone-15-pink-pdp-image-position-1a-pink-color.jpg"
         },{
             'id': 6,
             'name': 'iPhone 15 Pro',
             'price':'20000000',
-            'image': "https://www.google.com/aclk?sa=l&ai=DChcSEwj9srD08JCCAxVNyxYFHZ38CnUYABAJGgJ0bA&ase=2&gclid=EAIaIQobChMI_bKw9PCQggMVTcsWBR2d_Ap1EAQYBSABEgKxvvD_BwE&sig=AOD64_003wOBpiGh94HOk4mrEvCzhzpghA&ctype=5&nis=5&adurl&ved=2ahUKEwj9gaj08JCCAxWDl1YBHU4hDc8Qvhd6BQgBEIcB"
+            'image': "https://nsv.by/upload/image_resize/6cc/3c6/51699f3bf56835cbadc68bb63c0ab9b4.webp"
         }
     ]
+    if kw:
+        products = [p for p in products if p['name'].lower().find(kw.lower()) >= 0]
 
     return products
--- app/index.py
+++ app/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import dao
 
 app = Flask(__name__)
@@ -6,8 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    # trong thanh tìm kiếm người dùng nhập vào thì sẽ get được key
+    kw = request.args.get("kw")
+
     cates = dao.get_categories()
-    prods = dao.get_products()
+    prods = dao.get_products(kw)
     # tạo biến để lấy database, import thư mục chứa data
     return render_template('index.html', categories=cates, products=prods)
 
